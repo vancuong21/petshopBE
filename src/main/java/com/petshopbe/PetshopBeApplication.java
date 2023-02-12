837acc4064afc009c8a296d57efdccf61d45f01c
Vũ Văn Cường <cuongvv1121@gmail.com>
2023-02-12 23:20:38 +0700

    first commit

---
+++ src/main/java/com/petshopbe/PetshopBeApplication.java
@@ -0,0 +1,13 @@
+package com.petshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/petshopbe/PetshopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
